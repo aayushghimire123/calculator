7ccf43fe9ff9f2303eed764ca67f9ed2fa4fc15a
Aayush Ghimire <112367570+aayushghimire123@users.noreply.github.com>
2023-12-16 15:33:35 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let displayValue = '0';
+
+function updateDisplay() {
+    document.getElementById('display').innerText = displayValue;
+}
+
+function appendToDisplay(value) {
+    if (displayValue === '0' && value !== '.') {
+        displayValue = value;
+    } else {
+        displayValue += value;
+    }
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '0';
+    updateDisplay();
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue).toString();
+    } catch (error) {
+        displayValue = 'Error';
+    }
+    updateDisplay();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    updateDisplay();
+});
